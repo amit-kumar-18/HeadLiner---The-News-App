745eded6e67b18dbdc2870b424e5a30f4b603fa6
Amit Kumar <amitrawal316@gmail.com>
2023-04-14 18:48:22 +0530

    footer added and configed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,26 +1,38 @@
+import github from '../assets/images/github.svg'
+import linkedin from '../assets/images/linkedin.svg'
+import email from '../assets/images/email.svg'
+
 const Footer = () => {
   return (
     <footer className='footer'>
       <div className='content '>
-        <h1 className='logo__text has-text-white has-text-centered'>HeadLiner</h1>
         <div className='footer__links'>
-          <p className='is-size-5'>
-            HeadLiner is a React based News Application which fetches news using News-Catchers API.
-            You can read news of multiple genres like Word Affaires, Entertainment, Sports, Politics
-            and Business.
-          </p>
+          <h1 className='logo__text has-text-white has-text-centered'>HeadLiner</h1>
           <ul>
             <li>
-              <a href=''>GitHub</a>
+              <a href='https://github.com/amit-kumar-18' target='_blank'>
+                <img src={github} alt='GitHub' title='GitHub' />
+              </a>
             </li>
             <li>
-              <a href=''>LinkedIn</a>
+              <a href='https://www.linkedin.com/in/amit-kumar-86393722a' target='_blank'>
+                <img src={linkedin} alt='LinkedIn' title='LinkedIn' />
+              </a>
             </li>
             <li>
-              <a href=''>Email</a>
+              <a href='mailto:amitrawal316@gmail.com' target='_blank'>
+                <img src={email} alt='Email' title='Email' />
+              </a>
             </li>
           </ul>
         </div>
+        <hr />
+        <p className='has-text-black'>
+          powered by
+          <a href='https://newscatcherapi.com/' target='_blank'>
+            &lt;/newscatcher&gt;
+          </a>
+        </p>
       </div>
     </footer>
   )
